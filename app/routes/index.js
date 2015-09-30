5021ff23db422c814b2f4586aa3d03680173caea
Alyssa Horrocks & Alex Altieri <student@epicodus.com>
2015-09-30 16:00:45 -0700

    Add styling

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('neighborhood')
+    return Ember.RSVP.hash({
+      northwestNeighborhoods: this.store.findAll('neighborhood').then(function(neighborhoods) {
+        return neighborhoods.filterBy('name', 'NorthWest');
+      }),
+      northeastNeighborhoods: this.store.findAll('neighborhood').then(function(neighborhoods) {
+        return neighborhoods.filterBy('name', 'NorthEast');
+      }),
+      southwestNeighborhoods: this.store.findAll('neighborhood').then(function(neighborhoods) {
+        return neighborhoods.filterBy('name', 'SouthWest');
+      }),
+      southeastNeighborhoods: this.store.findAll('neighborhood').then(function(neighborhoods) {
+        return neighborhoods.filterBy('name', 'SouthEast');
+      }),
+      noPoNeighborhoods: this.store.findAll('neighborhood').then(function(neighborhoods) {
+        return neighborhoods.filterBy('name', 'North Portland');
+      }),
+    });
   }
 });
