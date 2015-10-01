c903ef78b919d59ffef044d7884b2d6413a387f1
alex altieri & alyssa horrocks <student@epicodus.com>
2015-10-01 15:11:56 -0700

    Add styling

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -10,19 +10,26 @@ export default Ember.Component.extend({
 
       var options = {
         center: this.get('map').center(neighborhood.get('latitude'), neighborhood.get('longitude')),
-        zoom: 15,
+        zoom: 14,
       };
 
       var ourMap = this.get('map').findMap(container, options);
-      var marker = undefined;
 
       bars.forEach(function(bar) {
+        var marker = undefined;
         var lat = bar.get('latitude');
         var lng = bar.get('longitude');
         marker = new google.maps.Marker({
           position: new google.maps.LatLng(lat, lng),
           map: ourMap,
           title: (bar.get('name')),
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+          content: marker.title
+        });
+        marker.addListener('click', function() {
+          infowindow.open(ourMap, marker);
         });
       });
     }
